b5d53a59c0a88954410d9a1fb7b77053cc16d2bc
BTAxis <BTAxis@gmail.com>
2012-03-09 16:07:01 +0100

    * Mockup of planet attribute infrastructure.
    Conflicts:
    
    	dat/commodities/food.lua

---
+++ dat/commodities/food.lua
@@ -0,0 +1,17 @@
+function calc_supplyDemand (population,radius,fields,lifestyle)
+   -- Mockup of interaction with planetary attributes.
+   -- Scripts are passed a planet to run on, and poll that planet for attributes.
+   -- The values of the attributes can be used in calculations. 
+   --
+   -- pollution = p:attribute("Pollution")
+   -- crops = p:attribute("Bumper Crops")
+   -- famine = p:attribute("Famine")
+   -- supply = supply - 0.5 * pollution
+   -- supply = supply + 2 * crops
+   -- demand = 2 -- All planets always consume food.
+   -- demand = demand + famine * 1.5
+   -- return supply, demand
+
+   -- Test values.
+   return 2, 0
+end
