efc2552bc7ad10f0c8ae37a6789e9f7ee283f93a
Jack Greiner <jack@emoss.org>
2020-10-01 18:18:41 -0400

    Basic AppImage packaging script.

--- extras/appimage/buildAppImage.sh
+++ extras/appimage/buildAppImage.sh
@@ -6,74 +6,32 @@
 #
 # For more information, see http://appimage.org/
 
+# Set ARCH of AppImage
 export ARCH=$(arch)
 
+# Set App and VERSION variables
 APP=Naev
-LOWERAPP=naev
+VERSION=$(git rev-parse --short HEAD)
 
-GIT_REV=$(git rev-parse --short HEAD)
-echo $GIT_REV
+# Set DESTDIR for ninja
+export DESTDIR=$(pwd)/dist/$APP.AppDir
 
-RELEASE_VERSION=$(git describe --tags)
+# Build Naev with prefix /usr with no docs
+meson setup build --buildtype release --prefix=/usr -Dconfigure_doc=false
 
-export DESTDIR=$(pwd)/$APP.AppDir
-
-meson setup build --buildtype release --prefix="/usr/" -Dconfigure_doc=false
+# Build and install Naev to DESTDIR
+ninja -C build
 ninja -C build install
 
-wget -q https://github.com/probonopd/AppImages/raw/master/functions.sh -O ./functions.sh
-#Remove line that should not be in functions.sh
-sed -i -r 's/set -x//' functions.sh
-#Silence wget
-sed -i 's/wget/wget -q/' functions.sh
-. ./functions.sh
+# Get linuxdeploy's AppImage
+mkdir linuxdeploy-bin
+wget https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
+chmod +x linuxdeploy-x86_64.AppImage
+mv linuxdeploy-x86_64.AppImage linuxdeploy-bin/
 
-cd $(pwd)/$APP.AppDir
+# Run linuxdeploy and generate an AppDir, then generate an AppImage
+./linuxdeploy-bin/linuxdeploy-x86_64.AppImage --appdir $(pwd)/dist/$APP.AppDir --output appimage
 
-########################################################################
-# Copy desktop and icon file to AppDir for AppRun to pick them up
-########################################################################
-
-get_apprun
-get_desktop
-get_icon
-
-########################################################################
-# Copy in the dependencies that cannot be assumed to be available
-# on all target systems
-########################################################################
-
-copy_deps
-
-########################################################################
-# Delete stuff that should not go into the AppImage
-########################################################################
-
-# Delete dangerous libraries; see
-# https://github.com/probonopd/AppImages/blob/master/excludelist
-
-delete_blacklisted
-
-########################################################################
-# Determine the version of the app; also include needed glibc version
-########################################################################
-
-VERSION=${RELEASE_VERSION}
-export VERSION
-
-########################################################################
-# Patch away absolute paths
-########################################################################
-
-patch_usr
-
-########################################################################
-# AppDir complete
-# Now packaging it as an AppImage
-########################################################################
-
-# Exit appimage
-cd ..
-
-mkdir -p out/
-generate_type2_appimage
+# Move AppImage to dist/ and mark as executable
+chmod +x $APP-$VERSION-$ARCH.AppImage
+mv $APP-$VERSION-$ARCH.AppImage dist/
